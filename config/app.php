6700279f4a7c51e0a6ca2f5bcbfae100408793d5
erikbruinsma <bruinsmareclame@me.com>
2020-04-08 10:27:51 +0200

    Fideloper update

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Fideloper\Proxy\TrustedProxyServiceProvider::class,
     ],
 
     /*
