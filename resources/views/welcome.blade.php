d1612ecc77c5a082de98d7b44e5b70f295c25ae4
erikbruinsma <bruinsmareclame@me.com>
2020-04-10 01:10:53 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Laravel Cached <?PHP time(); ?>
+                   Laravel Cached <?PHP echo time(); ?>
                 </div>
 
                 <div class="links">
